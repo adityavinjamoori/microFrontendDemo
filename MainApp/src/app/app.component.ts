5764570ab6a1b4e8ae7a8cbea045a11e283d6e7d
adithyavinjamoori <vin.adithya@gmail.com>
2021-07-12 22:32:11 +0530

    Pushing changes to main

--- MainApp/src/app/app.component.ts
+++ MainApp/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Footer } from 'cbre-product-footer';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'Product-Management';
+
+  ngOnInit(){
+    ReactDOM.render(React.createElement(Footer), document.getElementById("footer-container"));
+
+  }
 }
